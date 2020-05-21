50e557db8da531411db87a2db7534bdaab809fe6
xeusteerapat <xeus085@gmail.com>
2020-05-21 21:43:13 +0700

    switch styling to tailwind

--- client/src/index.js
+++ client/src/index.js
@@ -1,15 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter } from 'react-router-dom';
-import { ThemeProvider } from '@chakra-ui/core';
-import App from './components/App';
-import './index.css';
 
-ReactDOM.render(
-  <ThemeProvider>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </ThemeProvider>,
-  document.getElementById('root')
-);
+const App = () => {
+  return <div>hi</div>;
+};
+
+ReactDOM.render(<App />, document.getElementById('root'));
